7d8bd921d53396ecb2722c3c6787e150d6008081
Gunjan Kumar <gunjanraj321@gmail.com>
2023-10-17 17:11:23 +0530

    working

--- script.js
+++ script.js
@@ -1,120 +1,95 @@
-window.addEventListener('load', async () => {
-    // Fetch data from crudcrud
-    try{
-        await fetchDataFromCrudCrud();
-    }
-    catch (err) {
-        console.error('Error fetching Data', err);
-    }
-    
-});
+const todoApiUrl = "https://crudcrud.com/api/abe068a8862147e7a776175b4d02bef4/task"
+const completedApiUrl = "https://crudcrud.com/api/abe068a8862147e7a776175b4d02bef4/tasks"
 
-async function fetchDataFromCrudCrud  () {
+async function fetchDataFromCrudCrud() {
     try {
-        const res = await axios.get("https://crudcrud.com/api/05e06d32e2a74c3b90ca06c734ffc702/data");
-        console.log('API',res.data)
+        const todoResponse = await axios.get(todoApiUrl);
+        const completedResponse = await axios.get(completedApiUrl);
 
-        // Split data into completed and uncompleted tasks
-        const completedTasks = res.data.filter(task => task.completed);
-        const uncompletedTasks = res.data.filter(task => !task.completed);
+        const todoList = document.getElementById('toDoList')
+        const completedList = document.getElementById('taskDone')
 
+        todoList.innerHTML = '<h2>Todo Task</h2>'
+        completedList.innerHTML = "<h2>Completed Task</h2>"
 
-        for (let i = 0; i < uncompletedTasks.length; i++) {
-            ShowUserOnScreen(uncompletedTasks[i], 'ul1');
+        const todoTasks = todoResponse.data;
+        const completedTasks = completedResponse.data;
+
+        todoTasks.forEach((task) => {
+            const taskItem = createTaskElement(task,true);
+            todoList.appendChild(taskItem);
+        })
+
+        completedTasks.forEach((task) => {
+            const taskItem = createTaskElement(task,false)
+            completedList.appendChild(taskItem)
+        })
+
+    } catch (err) {
+        console.error('Error in fetching data:', err);
+    }
+}
+
+//function to create a task element with 'done' button
+
+function createTaskElement(task,isTodo){
+    const taskItem = document.createElement('div')
+    const list = isTodo ? "todoList" : "completedList";
+    taskItem.innerHTML = `
+    <span>${task.taskName} - ${task.taskDescription}</span>
+    <button onclick="moveToCompleted('${task._id}')">Done</button>
+    <button onclick="deleteTask('${task._id}', '${list}')">Delete</button>
+  `;
+    return taskItem;
+}
+
+async function addTask(){
+    const taskName = document.getElementById('task').value;
+    const taskDescription = document.getElementById('desc').value;
+
+    if(taskName){
+        try{
+            await axios.post(todoApiUrl, {
+                taskName,
+                taskDescription,
+            });
+            fetchDataFromCrudCrud();
         }
-        for (let i = 0; i < completedTasks.length; i++) {
-            ShowUserOnScreen(completedTasks[i], 'ul2');
+        catch(error){
+            console.error('error adding Task',error)
         }
-    } catch (err) {
-        console.error('Error updating task on the server:', err);
     }
 }
 
-async function crudoperation(event) {
-    event.preventDefault();
-    const task = event.target.task.value;
-    const desc = event.target.desc.value;
+async function deleteTask(taskId, list) {
+    const apiUrl = list === 'todoList' ? todoApiUrl : completedApiUrl;
 
-    console.log('task',task)
-    console.log('desc',desc)
-
-    const obj = {
-        task,
-        desc,
-        completed: false
-    };
-
-    try {
-        const res = await axios.post("https://crudcrud.com/api/05e06d32e2a74c3b90ca06c734ffc702/data", obj);
-        ShowUserOnScreen(res.data,'ul1');
-        console.log(res);
-
-    } catch (err) {
-        console.error('Error updating task on the server:', err);
-    }
-}
-
-function createButton(text, clickHandler) {
-    const button = document.createElement('button');
-    button.textContent = text;
-    button.addEventListener('click', clickHandler);
-    return button;
-}
-
-async function markAsDone(obj, element) {
-    // Update the task as completed locally
-    obj.completed = true;
-
-    // Remove the "Done" and "Delete" buttons
-    element.innerHTML = `Task: ${obj.task}, Description: ${obj.desc} (Completed)`;
-
-    const completedTask = document.getElementById('ul2');
-    completedTask.appendChild(element);
-
-    try {
-        // Update the task as completed on the server
-        await axios.put(`https://crudcrud.com/api/05e06d32e2a74c3b90ca06c734ffc702/data/${obj._id}`, obj);
-        console.log('Task updated on the server');
-    } catch (err) {
-        console.error('Error updating task on the server:', err);
-    }
-}
-
-
-function ShowUserOnScreen(obj, listId) {
-    const parentElem1 = document.getElementById(listId);
-    const childElem = document.createElement('li');
-
-    const taskElem =document.createElement('span')
-    taskElem.textContent = `Task: ${obj.task}`;
-
-    const descElem = document.createElement('span')
-    descElem.textContent =`Description:  ${obj.desc}`
-
-    childElem.appendChild(taskElem);
-    childElem.appendChild(descElem);
-
-    // Check if task is completed and remove buttons if it is
-    if (obj.completed) {
-        parentElem1.appendChild(childElem);
-    } else {
-        const doneBtn = createButton('Done', () => markAsDone(obj, childElem));
-        const delBtn = createButton('Delete', () => deleteTask(obj, childElem));
-        childElem.appendChild(doneBtn);
-        childElem.appendChild(delBtn);
-        parentElem1.appendChild(childElem);
-    }
-    
-}
-
-async function deleteTask(obj, element) {
-    const parentElem1 = obj.completed ? document.getElementById('ul2') : document.getElementById('ul1');
-    parentElem1.removeChild(element);
     try {
         // Delete from the server
-        await axios.delete(`https://crudcrud.com/api/05e06d32e2a74c3b90ca06c734ffc702/data/${obj._id}`);
-        console.log('Task deleted from the server');
-    } catch (err) {
-        console.error('Error updating task on the server:', err);
+        await axios.delete(`${apiUrl}/${taskId}`);
+        fetchDataFromCrudCrud();
+        }
+        catch (err) {
+        console.error('Error deleting task on the server:', err);
     }
 }
+
+async function moveToCompleted(taskId) {
+    try {
+        const task = await axios.get(`${todoApiUrl}/${taskId}`)
+
+        await axios.post(completedApiUrl, {
+            taskName: task.data.taskName,
+            taskDescription: task.data.taskDescription
+        })
+        deleteTask(taskId,"todoList")
+    }catch(err){
+        console.log("error moving to completed",err)
+    }
+}
+
+fetchDataFromCrudCrud();
+
+document
+    .querySelector('button[type="submit"]')
+    .addEventListener("click",addTask)
